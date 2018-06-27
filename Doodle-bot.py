bcac25f29ee3415b9501db5905ef06223ffa4649
silicWulf <me@siliconwolf.pw>
2018-06-26 22:23:19 -0400

    fix this one too

--- Doodle-bot.py
+++ Doodle-bot.py
@@ -17,12 +17,19 @@ startup_extensions = ['moderation', 'general', 'on_message_stuff']
 client.data = {'test': 'test object'}
 
 
-def stafforcomm(inp):
-    if ifcomm(inp):  # less redundant
-        return True
-    # if client.data[str(inp.guild)]['stfrole'] == discord.utils.get(inp.author.roles, id=self.client.staffrole.id):
-    eg = yield from env.get('{}.stfrole'.format(inp.guild.id))
-    if int(eg) in [role.id for role in inp.author.roles]:  # more efficient, gets all role ids
+async def stafforcomm(self, inp):
+    if '[no]' not in inp.author.display_name:
+        if ifcomm(self, inp):
+            return True
+
+    sst = await env.get('{}_stfrole'.format(str(inp.guild.id)))
+    try:
+        if sst == 'variable does not exist':  # if var error from server
+            return False  # since there is no mod role to compare against anyways
+        server_stfrole = int(sst)
+    except ValueError:
+        raise ValueError(f'server_stfrole can\'t be assigned to {sst}, probably not a real id. maybe define a staffrole with an id?')
+    if server_stfrole == discord.utils.get(inp.author.roles, id=server_stfrole).id:  # check to make sure the ids are the same
         return True
     else:
         return False
