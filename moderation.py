e986b99f136630ae2fcb9d07f3dedd446721e499
silicWulf <me@siliconwolf.pw>
2018-06-27 18:18:37 -0400

    .mooveee

--- moderation.py
+++ moderation.py
@@ -115,6 +115,33 @@ class Staff():
         await ctx.message.guild.me.edit(nick=name)
         await ctx.channel.send('Nickname is now "' + name + '"')
 
+        
+    @commands.command(brief='temp pos: change another\'s tag')
+    async def setusertag(self, ctx):
+        msg = ctx.message
+        content = msg.content
+        author = msg.author
+        is_sfc = await role_checks.stafforcomm(self, msg)
+        if is_sfc:
+            splits = content.split(' ')
+            print(splits)
+            if len(splits) < 3:
+                await ctx.channel.send("Invalid syntax. Usage: `=setusertag @username <tag>`")
+            else:
+                newtag = ' '.join(splits[2:])
+                if not splits[1].startswith('<@') and not splits[1].endswith('>'):
+                    await ctx.channel.send("Invalid syntax (bad mention). Usage: `=setusertag @username <tag>`")
+                else:
+                    to_mem = msg.guild.get_member(int(splits[1][2:-1].replace('!', '')))
+                    print(to_mem)
+                    if to_mem is None:
+                        await ctx.channel.send("Not a valid mention. Usage: `=setusertag @username <tag>`")
+                    else:
+                        await env.set("{}_tag_{}".format(ctx.message.guild.id, ctx.message.author.id), newtag)
+                        await ctx.channel.send("Set {}'s tag.".format(to_mem.display_name))
+        else:
+            await ctx.channel.send("You are not permitted to use this command.")
+
 
 def setup(client):
     client.add_cog(Staff(client))
