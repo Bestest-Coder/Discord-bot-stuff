a2b6abadfa0d5f0dacb77cb71457d77bf772aac7
Bestest-Coder <37045132+Bestest-Coder@users.noreply.github.com>
2018-09-08 13:49:58 -0400

    removed some stuff

--- on_message_stuff.py
+++ on_message_stuff.py
@@ -95,29 +95,5 @@ class on_msg():
                 await self.client.delete_message(message)
     '''
 
-    '''
-        #if message.content.startswith('Ruddy'):
-            #msg = 'all hail our lord and savior'.format(message)
-            #await self.client.send_message(message.channel, msg)
-            #time.sleep(600)
-
-        #if message.content.startswith('gaster'):
-            #msg = 'Ban gaster'.format(message)
-            #await self.client.send_message(message.channel, msg)
-
-        #if message.content.startswith('billy mays'):
-            #msg = 'Drink Oxiclean!'.format(message)
-            #await self.client.send_message(message.channel, msg)
-
-        #if message.content.startswith('hayden'):
-            #msg = "We do not speak it's name!".format(message)
-            #await self.client.send_message(message.channel, msg)
-
-        #if message.content.startswith('gandy'):
-            #msg = 'Theif of the least dank memes'.format(message)
-            #await self.client.send_message(message.channel, msg)
-    '''
-
-
 def setup(client):
     client.add_cog(on_msg(client))
