1276054ebc9f6e89b9a9947e01cf3c501ccdc9d8
Bestest-Coder <37045132+Bestest-Coder@users.noreply.github.com>
2018-11-07 20:47:05 -0500

    error handling on pfp

--- general.py
+++ general.py
@@ -35,9 +35,12 @@ class General():
 
     @commands.command(brief="get's the users avatar/pfp")
     async def pfp(self, ctx, *, user: discord.User):
-        em = discord.Embed()
-        em.set_image(url=user.avatar_url_as(static_format='png'))
-        await ctx.channel.send(embed=em)
+        try:
+            em = discord.Embed()
+            em.set_image(url=user.avatar_url_as(static_format='png'))
+            await ctx.channel.send(embed=em)
+        except Forbidden:
+            await ctx.channel.send("Error: cannot send embeds")
 
     @commands.command()
     async def void(self, ctx):
