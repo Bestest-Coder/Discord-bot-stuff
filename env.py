0b8c296f83b64f500f4bb6d126b2c6333f33f25e
silicWulf <me@siliconwolf.pw>
2019-04-27 16:58:37 -0400

    Update env.py

--- env.py
+++ env.py
@@ -6,11 +6,11 @@ import pickle
 db = pickle.load(open("db.pkl", "rb"))
 
 async def set(name, value, tries=5, timeout=2):
-    db['bestest'][name] = value
+    db['bestest'][name] = str(value)
     pickle.dump(db, open("db.pkl", "wb"))
 
 async def get(name, tries=5, timeout=2):
     try:
-        return str(db['bestest'][name])
+        return db['bestest'][name]
     except KeyError:
         return 'variable does not exist'
